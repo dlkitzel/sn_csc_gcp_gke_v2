7b20cf616caecdc1883af1dbc25fc29e12599787
Derek Kitzelman <derek.kitzelman@gmail.com>
2024-07-10 12:54:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "project_id" {
   type        = string
 }
 
+variable "credentials" {
+  description = "The credentials which will be used to provision resources."
+  type        = string
+}
+
 variable "region" {
   description = "The region in which to provision resources."
   type        = string
